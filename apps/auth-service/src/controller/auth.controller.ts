73d566a25159dc0aef7fbda48e419ef3fbbd4bd3
Shakhawat-pro <ratulhossain2692004@gmail.com>
2025-08-13 02:53:37 +0600

    login user done

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData, verifyOtp } from "../utils/auth.helper";
 import bcrypt from "bcrypt"
 import prisma from "../../../../packages/libs/prisma";
-import { ValidationError } from "../../../../packages/error-handler";
-
-
+import { AuthError, ValidationError } from "../../../../packages/error-handler";
+import Jwt from "jsonwebtoken";
+import { setCookie } from "../utils/cookies/setCoookie";
 // Register a new user
 export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -30,7 +30,6 @@ export const userRegistration = async (req: Request, res: Response, next: NextFu
 };
 
 // verify user with otp
-
 export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, otp, name, password } = req.body;
@@ -58,6 +57,59 @@ export const verifyUser = async (req: Request, res: Response, next: NextFunction
             success: true,
             message: "User registered successfully!",
             user
+        });
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+// login user
+export const LoginUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return next(new ValidationError("Email and password are required!"));
+        }
+
+        const user = await prisma.users.findUnique({ where: { email } })
+
+        if (!user) return next(new AuthError("User does not exist!"));
+
+        const isPasswordValid = await bcrypt.compare(password, user.password!)
+
+        if (!isPasswordValid) {
+            return next(new AuthError("Invalid password!"));
+        }
+
+        // Generate access and refresh tokens
+        const accessToken = Jwt.sign(
+            { userId: user.id },
+            process.env.ACCESS_TOKEN_SECRET! as string,
+            { expiresIn: "15m" }
+        );
+        const refreshToken = Jwt.sign(
+            { userId: user.id },
+            process.env.REFRESH_TOKEN_SECRET! as string,
+            { expiresIn: "1d" }
+        );
+
+        // store the refresh and access token in an httpOnly cookie
+        setCookie(res, "refreshToken", refreshToken)
+        setCookie(res, "accessToken", accessToken)
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful!",
+            // user,
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name
+            },
+            accessToken,
+            refreshToken
         });
 
     } catch (error) {
--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { userRegistration, verifyUser } from "../controller/auth.controller"
+import { LoginUser, userRegistration, verifyUser } from "../controller/auth.controller"
 
 
 const router = Router()
 
 router.post("/user-registration", userRegistration)
 router.post("/verify-user", verifyUser)
+router.post("/login-user", LoginUser)
 
 
 export default router
