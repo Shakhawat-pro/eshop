d22f1d8f0fd468c62e5e97d34bf6456835134272
MD. Shakhawat Hossain <ratulhossain2692004@gmail.com>
2026-04-19 13:03:29 +0600

    enhance ImagePlaceHolder component with file handling and preview features; integrate into CreateProduct page

--- apps/seller-ui/src/components/Shared/ImagePlaceHolder/ImagePlaceHolder.tsx
+++ apps/seller-ui/src/components/Shared/ImagePlaceHolder/ImagePlaceHolder.tsx
@@ -1,7 +1,9 @@
+import { Pencil, WandSparkles, X } from 'lucide-react';
+import Image from 'next/image';
 import { useState } from 'react';
 
 type Props = {
-    size: number;
+    size: number | string;
     small?: boolean;
     onImageChange: (file: File | null, index: number) => void;
     onRemove?: (index: number) => void;
@@ -11,12 +13,60 @@ type Props = {
 }
 
 const ImagePlaceHolder = ({ size, small, onImageChange, onRemove, defaultImage, setOpenImageModal, index }: Props) => {
+
     const [imagePreview, setImagePreview] = useState<string | null>(defaultImage || null);
 
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setImagePreview(URL.createObjectURL(file));
+            onImageChange(file, index);
+        }
+    };
 
     return (
-        <div>
-            ImagePlaceHolder 
+        <div className={`relative ${small ? "h-[180px]" : "h-[450px]"} w-full cursor-pointer bg-[#1e1e1e] border border-gray-600 rounded-lg flex flex-col items-center justify-center`}>
+            <input type='file'
+                accept='image/*'
+                className='hidden'
+                id={`image-upload-${index}`}
+                onChange={handleFileChange}
+            />
+            {imagePreview ? (
+                <>
+                    <button type='button'
+                        onClick={() => {
+                            setImagePreview(null);
+                            onRemove ? onRemove(index) : onImageChange(null, index);
+                        }}
+                        className='absolute top-2 right-2 bg-red-500 text-white rounded-full p-1 z-10'
+                    >
+                        <X size={16} />
+                    </button>
+                    <button type='button' onClick={() => setOpenImageModal(true)}
+                        className='absolute top-3 right-[70px] rounded bg-blue-500 shadow-lg cursor-pointer'>
+                        <WandSparkles size={16} color='white' />
+                    </button>
+                </>
+            ) : (
+                <label htmlFor={`image-upload-${index}`} className='absolute top-3 right-3 p-2 rounded bg-slate-700 shadow-lg cursor-pointer'>
+                    <Pencil size={16} color='white' />
+                </label>
+            )}
+
+            {imagePreview ? (
+                <Image src={imagePreview} alt={`Image Preview ${index}`} width={200} height={200} className='object-cover h-full w-full rounded-md' />
+            ) : (
+                <>
+                    <p className={`text-gray-400 ${small ? "text-xl" : "text-4xl"} font-semibold`}>
+                        {size}
+                    </p>
+                    <p className={`text-gray-400 ${small ? "text-sm" : "text-base"} pt-2 text-center`}>
+                        Please choose an Image <br />
+                        according to the expected ratio.
+                    </p>
+                </>
+            )}
         </div>
     );
 };
--- apps/seller-ui/src/components/page/Product/CreateProduct/CreateProduct.tsx
+++ apps/seller-ui/src/components/page/Product/CreateProduct/CreateProduct.tsx
@@ -1,15 +1,50 @@
 "use client";
 import BreadCrumbAndHeader from '@/components/Shared/BreadCrumbAndHeader';
 import ImagePlaceHolder from '@/components/Shared/ImagePlaceHolder/ImagePlaceHolder';
+import { useState } from 'react';
 
 import { useForm } from 'react-hook-form';
 
 const CreateProduct = () => {
+    const [openImageModal, setOpenImageModal] = useState(false);
+    const [isChanged, setIsChanged] = useState(false);
+    const [images, setImages] = useState<(File | null)[]>([null]);
+    const [loading, setLoading] = useState(false);
 
     const { register, control, watch, setValue, handleSubmit, formState: { errors }, } = useForm()
 
     const onSubmit = (data: any) => {
         console.log(data);
+    }
+
+    const handleImageChange = (file: File | null, index: number) => {
+        const updatedImages = [...images];
+        updatedImages[index] = file;
+
+        if (index === images.length - 1 && file) {
+            updatedImages.push(null);
+        }
+
+        setImages(updatedImages);
+        // setValue('images', updatedImages.filter(img => img !== null));
+        setValue('images', updatedImages);
+    }
+
+    const handleRemoveImage = (index: number) => {
+        setImages((prevImages) => {
+            const updatedImages = [...prevImages];
+
+            if (index === -1) {
+                updatedImages[0] = null;
+            } else {
+                updatedImages.splice(index, 1);
+            }
+            if (!updatedImages.includes(null) && updatedImages.length < 8) {
+                updatedImages.push(null);
+            }
+            return updatedImages;
+        });
+        setValue('images', images);
     }
 
 
@@ -25,9 +60,15 @@ const CreateProduct = () => {
             <form>
                 {/* Content Layout */}
                 <div className='py-4 w-full flex gap-6'>
-                    {/* Left Side - Image upload Section */}                  
+                    {/* Left Side - Image upload Section */}
                     <div className='w-full max-w-[35%] bg-[#1e1e1e] rounded-md p-4'>
-                         <ImagePlaceHolder size={200} onImageChange={() => { }} setOpenImageModal={() => { }} />
+                        <ImagePlaceHolder
+                            size="765 x 850"
+                            small={false}
+                            setOpenImageModal={setOpenImageModal}
+                            onImageChange={handleImageChange}
+                            onRemove={handleRemoveImage}
+                        />
                     </div>
                 </div>
             </form>
